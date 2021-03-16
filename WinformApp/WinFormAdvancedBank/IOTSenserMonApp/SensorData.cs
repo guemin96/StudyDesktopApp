bfbae8e3be83acaf60f27535cfa68a54ba4ac4d8
guemin96 <guemin12@naver.copm>
2021-03-16 12:34:56 +0900

    0316 오전커밋

---
+++ WinformApp/WinFormAdvancedBank/IOTSenserMonApp/SensorData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IOTSenserMonApp
+{
+    internal class SensorData
+    {
+        public DateTime current { get; set; } // 현재 시간
+        public int Value { get; set; } // 센서 값
+
+        public bool SimulFlag { get; set; } // 시뮬레이션 여부
+        public SensorData(DateTime current, int value, bool simulFlag)
+        {
+            this.current = current;
+            this.Value = value;
+            SimulFlag = simulFlag;
+        }
+    }
+}
