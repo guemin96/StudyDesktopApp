98138cf8fa04fa95c8d7758df1c0f59ae82962a6
guemin96 <guemin12@naver.copm>
2021-03-23 09:33:30 +0900

    커밋

--- WPFApp/WpfAdvBank/WpfPracticeApp/MainMenu.xaml.cs
+++ WPFApp/WpfAdvBank/WpfPracticeApp/MainMenu.xaml.cs
@@ -18,9 +18,9 @@ namespace WpfPracticeApp
     /// <summary>
     /// Page1.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class Page1 : Page
+    public partial class MainMenu : Page
     {
-        public Page1()
+        public MainMenu()
         {
             InitializeComponent();
         }
